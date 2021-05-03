5148e22e287840a84e4d38703d8f967305511337
crazyliu-1982 <python_crazyliu@sina.com>
2021-05-04 07:19:54 +0800

    我的第一次提交
    Signed-off-by: crazyliu-1982 <python_crazyliu@sina.com>

---
+++ study/src/main/java/com/course/testng/basicAciton.java
@@ -0,0 +1,12 @@
+package com.course.testng;
+
+import org.testng.annotations.Test;
+
+public class basicAciton {
+
+    @Test
+    public void testcase1(){
+        System.out.println("测试用例1");
+
+    }
+}
